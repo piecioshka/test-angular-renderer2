9e0cc2d8cee44587c2f9ed5bcc31f15382421482
Piotr Kowalski <piecioshka@gmail.com>
2019-03-05 17:47:48 +0100

    Inject Renderer2 to testing it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-angular-renderer2';
+export class AppComponent implements OnInit {
+
+  @ViewChild('item') markElement = null;
+  @ViewChild('item2') markElement2 = null;
+
+  constructor(
+    private renderer: Renderer2,
+  ) { }
+
+  ngOnInit() {
+    this.handle(this.markElement.nativeElement);
+    this.handle(this.markElement2.nativeElement);
+  }
+
+  handle(domElement) {
+    this.renderer.addClass(domElement, 'mark');
+    this.renderer.listen(domElement, 'click', (evt) => {
+      console.log(evt.target);
+    });
+  }
+
 }
