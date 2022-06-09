00d3df784c62f8d75a9dc30a8e25c8a92673f1d4
Piotr Kowalski <piecioshka@gmail.com>
2022-06-09 22:47:33 +0200

    Update deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, Renderer2, OnInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  Renderer2,
+  OnInit,
+  ViewChild,
+  DebugElement,
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -6,19 +12,19 @@ import { Component, Renderer2, OnInit, ViewChild } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  @ViewChild("item") markElement = null;
-  @ViewChild("item2") markElement2 = null;
+  @ViewChild("item1") markElement1: DebugElement | null = null;
+  @ViewChild("item2") markElement2: DebugElement | null = null;
 
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.handle(this.markElement.nativeElement);
-    this.handle(this.markElement2.nativeElement);
+    this.handle(this.markElement1?.nativeElement);
+    this.handle(this.markElement2?.nativeElement);
   }
 
-  handle(domElement) {
+  private handle(domElement: HTMLElement) {
     this.renderer.addClass(domElement, "mark");
-    this.renderer.listen(domElement, "click", (evt) => {
+    this.renderer.listen(domElement, "click", (evt: MouseEvent) => {
       console.log(evt.target);
     });
   }
